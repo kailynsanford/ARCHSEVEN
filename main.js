1406e7e4c91a20c689e6d2f9107cca9122b2ba28
Doliousis <150724990+Doliousis@users.noreply.github.com>
2024-02-29 12:40:44 -0800

    mainchange3

--- main.js
+++ main.js
@@ -1,3 +1,59 @@
+import { Player } from './player.js';
+import { InputHandler } from './input.js';
+
+window.addEventListener('load', function(){
+    const canvas = document.getElementById('canvas1');
+    const ctx=canvas.getContext('2d');
+    canvas.width = 500;
+    canvas.height = 500;
+     // Create an image element
+     const img = new Image();
+
+     // Set the source of the image
+     img.src = './images/purgatory.png';
+
+     // When the image loads, draw it on the canvas
+     img.onload = function() {
+         ctx.drawImage(img, 0, 0); // Draw the image at position (0, 0)
+     }
+     img.onerror = function() {
+        console.error("Error loading image:", img.src);
+    };
+    
+    img.onload = function() {
+        console.log("Image loaded successfully");
+        ctx.drawImage(img, 0, 0);
+    };
+    class Game {
+        constructor (width, height) {
+            this.width = width;
+            this.height = height;
+            this.player = new Player(this);
+            this.input = new InputHandler();
+        }
+        update(deltaTime){
+            this.player.update(this.input.keys, deltaTime);
+        }
+        draw(context){
+            this.player.draw(context);
+        }
+    }
+
+    const game = new Game(canvas.width, canvas.height)
+    console.log(game);
+    let lastTime = 0;
+
+    function animate(timeStamp){
+        const deltaTime = timeStamp - lastTime;
+        lastTime = timeStamp;
+        ctx.clearRect(0, 0, canvas.width, canvas.height)
+        game.update(deltaTime); 
+        game.draw(ctx)
+        requestAnimationFrame(animate);
+    }
+    animate(0);
+})
+
 window.addEventListener('load', function(){
     const canvas = document.getElementById('canvas1');
     const ctx = canvas.getContext('2d');
